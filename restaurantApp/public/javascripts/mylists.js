21ac0d47606efb95a8d2c8253cb1eadd4d9e63aa
helen-kim <hyejunk@andrew.cmu.edu>
2016-12-13 17:45:01 -0500

    all functionality working as expected; need to work on css and code clean up

--- restaurantApp/public/javascripts/mylists.js
+++ restaurantApp/public/javascripts/mylists.js
@@ -1,129 +1,6 @@
-// $(document).ready(function()  {
-//   delRest();
-
-//   // makes the API request to search for restaurants in city
-//   function getRestaurants() {
-//     $('#restCards').empty();
-//     function cb(data) {        
-//       console.log("cb: " + JSON.stringify(data));
-//     }
-            
-//     var auth = {
-//         //
-//         // Update with your auth tokens.
-//         //
-//         consumerKey : "S3yH_0hNDtkO4SYOZiFROA",
-//         consumerSecret : "NXQro3vSpGwgaMuL3N7OeUVi9xc",
-//         accessToken : "5QfCEAXQD-CqfMMv_hUg2VJbObcZV91z",
-//         // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
-//         // You wouldn't actually want to expose your access token secret like this in a real application.
-//         accessTokenSecret : "BAsY9N0jdeaL6QVPWcAqiBVP4FU",
-//         serviceProvider : {
-//             signatureMethod : "HMAC-SHA1"
-//         }
-//     };
-
-//     // var limit = '20';
-//     var sort = '0';
-//     var term = 'food'
-//     var near = $('#searchLocation').val();
-
-//     var accessor = {
-//         consumerSecret : auth.consumerSecret,
-//         tokenSecret : auth.accessTokenSecret
-//     };
-
-//     var parameters = [];
-//     // parameters.push(['limit', limit]);
-//     parameters.push(['sort', sort]);
-//     parameters.push(['term', term]);
-//     parameters.push(['location', near]);
-//     parameters.push(['callback', 'cb']);
-//     parameters.push(['oauth_consumer_key', auth.consumerKey]);
-//     parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
-//     parameters.push(['oauth_token', auth.accessToken]);
-//     parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
-
-//     var message = {
-//         'action' : 'https://api.yelp.com/v2/search',
-//         'method' : 'GET',
-//         'parameters' : parameters
-//     };
-
-//     OAuth.setTimestampAndNonce(message);
-//     OAuth.SignatureMethod.sign(message, accessor);
-
-//     var parameterMap = OAuth.getParameterMap(message.parameters);
-        
-//     $.ajax({
-//         'url' : message.action,
-//         'data' : parameterMap,
-//         'dataType' : 'jsonp',
-//         'jsonpCallback' : 'cb',
-//         'cache': true
-//     })
-//     .done(function(response) {
-//             for (var i = 0; i < response.businesses.length; i++) {
-//                 var name = response.businesses[i].name;
-//                 var address = "<p>"+response.businesses[i].location.display_address.join("</p><p>")+"</p>";
-//                 var rawadd = response.businesses[i].location.display_address;
-//                 var restimg = response.businesses[i].image_url;
-//                 var rating = Math.round(response.businesses[i].rating);
-
-
-
-//                 var card = "";
-//                 //add restaurant image
-//                 card += "<div class='ui centered card' id='rest"+i+"'><div class='image'><img src='"+restimg+"'></div>";
-//                 // add name
-//                 card += "<div class='content'><div class='header'>"+name+"</div>";
-//                 // add address
-//                 card += "<div class='description'>"+address+"</div></div>";
-//                 // add button
-
-//                 console.log(near);
-//                 var add = "addRest(\'" + encodeURIComponent(near) + "\',\'" + encodeURIComponent(name) + "\',\'" + encodeURIComponent(rawadd) + "\',\'" + encodeURIComponent(restimg) +"\')"
-//                 var del = "delRest(\'" + encodeURIComponent(near) + "\',\'" + encodeURIComponent(name) + "\',\'" + encodeURIComponent(rawadd) + "\',\'" + encodeURIComponent(restimg) +"\')"
-//                 card += "<div class='extra content'><div class='ui two buttons'><div class='ui bottom attached button' onclick="+add+"><i class='add icon'></i>Add</div>";
-//                 card += "<div class='ui bottom attached button' onclick="+del+"><i class='remove icon'></i>Remove</div></div></div></div>";
-//                 // card += "<div class='ui popup'><div class='header'>Rating</div><div class='ui star rating' data-rating='3'></div></div></div>";
-
-//                 // add card to cards list
-//                 $('#restCards').append(card);
-
-//            }
-//         }
-//     )
-//     .fail(function(jqXHR, textStatus, errorThrown) {
-//                         console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
-//             }
-//     );
-//     $("form").trigger("reset");
-//     event.preventDefault();
-//   }
-// });
- 
-// // handles possible keypress GUI for users wanting to search using enter key
-// $(function(){
-//   $('#sf').keypress(function(e){
-//     if(e.which == 13) {
-//       getRestaurants();
-//       e.preventDefault();
-//     }
-//   });
-// });
-
 $(document).ready(function() {
   showLists();
-  $('.ui.accordion').accordion();
-  // $(".ui.accordion .title:eq(0)").on("click", function() {
-  //   console.log("triggered");
-  // });
 });
-
-function triggered(number) {
-  console.log(number);
-}
 
 function showLists() {
   console.log("showlists called");
@@ -136,27 +13,38 @@ function showLists() {
             success:function(result){
                 console.log("Successfully retrieved item");
                 $('#lists').html(result);
-                var info = sortinfo();
-
-                $('#lists').html(info);
+                $('.ui.accordion').accordion();
             }
         });
 }
 
 function sortinfo() {
-  var jsonstr = $('#obj').text();
-                var data = JSON.parse(jsonstr);
+  var info = [{"_id":"58504bb7f4d5417e30810b4f","title":"Closter","restaurant":{"name":"Red Maple Restaurant","address":"328 Harrington Ave,Closter, NJ 07624","img":"https://s3-media3.fl.yelpcdn.com/bphoto/IWxr55bf0WIbb7C37utLTA/ms.jpg"},"username":"new"},{"_id":"58504bbcf4d5417e30810b50","title":"Closter","restaurant":{"name":"Homung Nangmyun","address":"570 Piermont Rd,Closter, NJ 07624","img":"https://s3-media3.fl.yelpcdn.com/bphoto/ubZ6XhyGs3_voOQhqq8QmQ/ms.jpg"},"username":"new"},{"_id":"58504bbef4d5417e30810b51","title":"Closter","restaurant":{"name":"Spring Restaurant","address":"221 Closter Dock Rd,Closter, NJ 07624","img":"https://s3-media2.fl.yelpcdn.com/bphoto/I5Y3-66tNB1Gv-5mZdy2oQ/ms.jpg"},"username":"new"},{"_id":"58504bd4f4d5417e30810b52","title":"Closter","restaurant":{"name":"The Barn In Closter","address":"570 Piermont Rd,Closter, NJ 07624","img":"https://s3-media3.fl.yelpcdn.com/bphoto/H0hzmjAmKWmlESeHq4xl5A/ms.jpg"},"username":"new"},{"_id":"58504bf1f4d5417e30810b53","title":"New York City","restaurant":{"name":"Los Tacos No.1","address":"75 9th Ave,Chelsea Market,Meatpacking District,New York, NY 10011","img":"https://s3-media2.fl.yelpcdn.com/bphoto/FU44TYl3PzXsE06G4W5aog/ms.jpg"},"username":"new"},{"_id":"58504bf3f4d5417e30810b54","title":"New York City","restaurant":{"name":"Cheeky Sandwiches","address":"35 Orchard St,Chinatown,New York, NY 10002","img":"https://s3-media1.fl.yelpcdn.com/bphoto/IwgJEZEA9nK77KK6FaHZng/ms.jpg"},"username":"new"},{"_id":"58504bf5f4d5417e30810b55","title":"New York City","restaurant":{"name":"Hudson Eats At Brookfield Place","address":"225 Liberty St,2nd Level,Battery Park,New York, NY 10281","img":"https://s3-media4.fl.yelpcdn.com/bphoto/vTtHHUhk8pPVjarNIKAM_w/ms.jpg"},"username":"new"},{"_id":"58504bfdf4d5417e30810b56","title":"Edgewater","restaurant":{"name":"Mitsuwa Marketplace","address":"595 River Rd,Edgewater, NJ 07020","img":"https://s3-media1.fl.yelpcdn.com/bphoto/0nkDiR3HO4NfPfp360DfhA/ms.jpg"},"username":"new"},{"_id":"58504c02f4d5417e30810b57","title":"Edgewater","restaurant":{"name":"Menya Sandaime","address":"1638 Parker Ave,Fort Lee, NJ 07024","img":"https://s3-media4.fl.yelpcdn.com/bphoto/-S2iKR-LDD_S4SnzKrGkSw/ms.jpg"},"username":"new"}]
 
-                var unique = {};
-                var distinct = [];
-                for( var i in data ){
-                 if( typeof(unique[data[i].title]) == "undefined"){
-                  distinct.push(data[i].title);
-                 }
-                 unique[data[i].title] = "";
-                }
+  var distinct = [];
+  var log = {};
+  for( var i in info ){
+   if( typeof(log[info[i].title]) == "undefined") {
+    distinct.push(info[i].title);
+    log[info[i].title] = [info[i].restaurant];
+   }
+   var rest = log[info[i].title];
+   rest.push(info[i].restaurant);
+   log[info[i].title] = rest;
+  }
 
-                var titles = "<p>"+distinct.join("</p><p>")+"</p>"
+  var rests = [];
+  for (var i in distinct) {
+    var all = {};
+    all["title"] = distinct[i];
+    all["restaurants"] = log[distinct[i]];
+    rests.push(all);
+  }
+
+  console.log(rests[0].title);
+  console.log(rests[0].restaurants);
+  console.log(rests[0].restaurants[0]);
+  console.log(rests[0].restaurants[0].name);
 }
 
 function delRest(title, name, address, img) {
@@ -176,7 +64,8 @@ function delRest(title, name, address, img) {
             data: { title: decodeURIComponent(title), restaurant: restinfo },
             success:function(result){
                 console.log("Successfully deleted item");
+                $('#results').append(result);
             }
         });
-        event.preventDefault();
+  location.reload();
 }
